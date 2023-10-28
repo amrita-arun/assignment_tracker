a70db9034d05a4683bce94a915ae69d010cf2f01
amrita-arun <amrita.arun2013@gmail.com>
2023-10-27 22:07:14 -0700

    started sidebar for taskdetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import TaskDetail from './components/TaskDetail'
 
 function App() {
   return (
     <>
       <h1 className="font-bold text-red-500">Hello friends</h1>
+      <TaskDetail />
     </>
   )
 }
--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const TaskDetail = () => {
   return (
-    <div>TaskDetail</div>
+    <>
+    <aside>
+      <div className="flex w-[300px] h-[1587px] opacity-80 bg-blue-300 rounded-[25px]">
+        <div className="flex w-[250px] h-[400px] bg-zinc-500 rounded-[25px]">
+
+        </div>
+      </div>
+    </aside>
+    </>
   )
 }
 
