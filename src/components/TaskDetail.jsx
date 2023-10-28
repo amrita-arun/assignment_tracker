52f7eb18c32ed8f118a161e5c20985c507f92265
amrita-arun <amrita.arun2013@gmail.com>
2023-10-27 22:36:52 -0700

    added blue sidebar box and brown timer box

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -4,8 +4,8 @@ const TaskDetail = () => {
   return (
     <>
     <aside>
-      <div className="flex w-[300px] h-[1587px] opacity-80 bg-blue-300 rounded-[25px]">
-        <div className="flex w-[250px] h-[400px] bg-zinc-500 rounded-[25px]">
+      <div className="pt-4 grid justify-items-stretch w-[300px] h-[1587px] opacity-80 bg-blue-300 rounded-[25px]">
+        <div className=" justify-self-center w-[250px] h-[400px] bg-zinc-500 rounded-[25px]">
 
         </div>
       </div>
