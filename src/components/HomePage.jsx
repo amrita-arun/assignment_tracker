9f64a470f692b7832c772caf650b5c46c37511d1
amrita-arun <amrita.arun2013@gmail.com>
2023-10-29 01:01:38 -0700

    added form popup

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import TaskDetail from './TaskDetail'
 import TaskComponent from './TaskComponent'
 import Timer from './Timer'
 import Datetime from 'react-datetime';
 import NewTask from './NewTask';
+import AddTaskForm from './AddTaskForm';
 
 
 const HomePage = () => {
   return (
     <>
-    <div>
-
-    </div>
     <div className='space-y-3 pt-6 pl-6 pr-6 justify-items-stretch h-[800px] opacity-80 bg-blue-100 rounded-[25px]'>
       <TaskComponent />
       <NewTask />
+      
     </div>
     
     {/*
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,18 +1,58 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
+import AddTaskForm from './AddTaskForm';
 
 const NewTask = () => {
-    const [task, setTask] = useState("");
-
-    function handleNewTask(event) {
-        <h1>HEY GUYS!</h1>
-    }
+    const [showForm, setShowForm] = useState(false);
     return (
     <div>
         <div className="h-[45px] opacity-40 bg-slate-400 rounded-[25px]">
             <div>
-                <button>NewTask</button>
+                <button onClick={() => setShowForm(true)}>NewTask</button>
             </div>
         </div>
+        <AddTaskForm isVisible={showForm} onClose={() => setShowForm(false)}>
+            <p class = "text-2xl font-bold text-center">Add Task</p>
+            <input 
+                type="text" 
+                class=" border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter your text here" />
+            <p class = "text-2xl font-bold text-center">Add Description of Task</p>
+            <input 
+                type="text" 
+                class="h-20 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter your text here"/>
+            <p class = "text-2xl font-bold text-center">Please Rank Priority</p>
+            <button id="DropdownButton" data-dropdown-toggle="DropdownMenu" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+            </svg>
+            </button>
+            <div id="DropdownMenu" class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+          <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="DropdownButton">
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input id="default-radio-4" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="default-radio-4" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Priority 1</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input checked id="default-radio-5" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="default-radio-5" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Priority 2</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <input id="default-radio-6" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="default-radio-6" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Priority 3</label>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <button id="submitButton" class="text-white bg-blue-300 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Submit<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+            </svg>
+        </button>
+        </AddTaskForm>
     </div>
     )
 }
